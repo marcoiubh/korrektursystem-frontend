9b7c2f1ad4b6571123427355f188b844d7a712f7
marcoiubh <marco.koll@iubh-fernstudium.de>
2022-11-15 22:38:45 +0100

    removed input validation

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,68 +1,30 @@
 import { useNavigate } from 'react-router-dom';
-import Joi from 'joi-browser';
 import React, { useState } from 'react';
 import Input from './subcomponents/input';
 import { loginUser } from './services/authenticationService';
 
-const Login = (props) => {
+const Login = () => {
   const [credentials, setCredentials] = useState({
     email: '',
     password: '',
   });
-  const [errors, setErrors] = useState({});
+
   const navigate = useNavigate();
-  const schema = {
-    email: Joi.string().required().email().label('E-mail'),
-    password: Joi.string().required().label('Password'),
-  };
-
-  const validate = () => {
-    const options = { abortEarly: false };
-    const { error } = Joi.validate(credentials, schema, options);
-    if (!error) return null;
-
-    for (let item of error.details)
-      errors[item.path[0]] = item.message;
-    return errors;
-  };
-
-  const validateProperty = ({ name, value }) => {
-    const obj = { [name]: value };
-    const schemaCopy = { [name]: schema[name] };
-    const { error } = Joi.validate(obj, schemaCopy);
-    return error ? error.details[0].message : null;
-  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
-    const errors = validate();
-    setErrors({ errors: errors || {} });
-    if (errors) return;
 
     try {
       await loginUser(credentials);
       navigate('/tickets');
       window.location.reload(false);
-    } catch (ex) {
-      if (ex.response && ex.response.status === 400) {
-        const errorsCopy = { ...errors };
-        errorsCopy.email = ex.response.data;
-        setErrors(errorsCopy);
-      }
-    }
+    } catch (ex) {}
   };
 
   const handleChange = ({ target: input }) => {
-    const errorsCopy = { ...errors };
-    const errorMessage = validateProperty(input);
-    if (errorMessage) errorsCopy[input.name] = errorMessage;
-    else delete errorsCopy[input.name];
-
     const credentialsCopy = { ...credentials };
     credentialsCopy[input.name] = input.value;
     setCredentials(credentialsCopy);
-    setErrors(errorsCopy);
   };
 
   return (
@@ -74,19 +36,14 @@ const Login = (props) => {
           name="email"
           value={credentials.email}
           onChange={handleChange}
-          error={errors.email}
         />
         <Input
           name="password"
           value={credentials.password}
           onChange={handleChange}
           type="password"
-          error={errors.password}
         />
-        <button
-          disabled={validate()}
-          className="btn btn-outline-primary small"
-        >
+        <button className="btn btn-outline-primary small">
           Login
         </button>
       </form>
--- src/components/ticketDetails.jsx
+++ src/components/ticketDetails.jsx
@@ -1,35 +1,14 @@
-import Joi from 'joi-browser';
 import React, { useState, useEffect } from 'react';
-import { Navigate, useParams, useNavigate } from 'react-router-dom';
-import Date from './subcomponents/date';
+import { useParams, useNavigate } from 'react-router-dom';
 import Input from './subcomponents/input';
-import Select from './subcomponents/select';
-import TextArea from './subcomponents/textarea';
-import Calendar from 'react-calendar';
-import {
-  getTicket,
-  saveTicket,
-  updateTicket,
-} from './services/ticketService';
+
+import { getTicket, updateTicket } from './services/ticketService';
 import config from '../config/config.json';
 
 const TicketDetails = (props) => {
   const navigate = useNavigate();
   const params = useParams();
   const [ticket, setTicket] = useState(config.ticket);
-
-  const [errors, setErrors] = useState({});
-
-  const schema = {
-    _id: Joi.toString(),
-    title: Joi.string().required().label('Title'),
-    module: Joi.array().min(1).label('Module'),
-    type: Joi.array().min(1).label('Type'),
-    source: Joi.array().min(1).label('Source'),
-    status: Joi.array().min(1).label('Status'),
-    comment: Joi.toString(),
-    priority: Joi.toString(),
-  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -39,41 +18,24 @@ const TicketDetails = (props) => {
     fetchData(params.id);
   }, []);
 
-  const validate = () => {
-    // const options = { abortEarly: false };
-    // const { error } = Joi.validate(ticket, schema, options);
-    // if (!error) return null;
-    // for (let item of error.details)
-    //   errors[item.path[0]] = item.message;
-    // return errors;
-  };
-
-  const validateProperty = ({ name, value }) => {
-    // const obj = { [name]: value };
-    // const schemaCopy = { [name]: schema[name] };
-    // const { error } = Joi.validate(obj, schemaCopy);
-    // return error ? error.details[0].message : null;
-  };
-
   const handleSave = async (e) => {
+    // prevents the browser to refresh
     e.preventDefault();
-    // const errors = validate();
-    // setErrors({ errors: errors || {} });
-    // if (errors) return;
+
+    // ticket gets updated if validation passes
     const result = await updateTicket(ticket);
     navigate('/tickets');
   };
 
+  // retrieves input from event.target.value
   const handleChange = ({ target: input }) => {
-    const errorsCopy = { ...errors };
-    const errorMessage = validateProperty(input);
-    if (errorMessage) errorsCopy[input.name] = errorMessage;
-    else delete errorsCopy[input.name];
-
+    // clones existing tickets
     const ticketCopy = { ...ticket };
+    // associates field values with field names
     ticketCopy[input.name] = input.value;
+
+    // stores states
     setTicket(ticketCopy);
-    setErrors(errorsCopy);
   };
 
   const handleCancel = (e) => {
@@ -89,54 +51,43 @@ const TicketDetails = (props) => {
           name="date"
           value={ticket.date}
           onChange={handleChange}
-          error={errors.date}
         />
         <Input
           name="priority"
           value={ticket.priority}
           onChange={handleChange}
-          error={errors.priority}
         />
         <Input
           name="title"
           value={ticket.title}
           onChange={handleChange}
-          error={errors.title}
         />
         <Input
           name="module"
           value={ticket.module}
           onChange={handleChange}
-          error={errors.module}
         />
         <Input
           name="type"
           value={ticket.type}
           onChange={handleChange}
-          error={errors.type}
         />
         <Input
           name="source"
           value={ticket.source}
           onChange={handleChange}
-          error={errors.source}
         />
         <Input
           name="status"
           value={ticket.status}
           onChange={handleChange}
-          error={errors.status}
         />
         <Input
           name="comment"
           value={ticket.comment}
           onChange={handleChange}
-          error={errors.comment}
         />
-        <button
-          disabled={validate()}
-          className="btn btn-outline-primary small m-2"
-        >
+        <button className="btn btn-outline-primary small m-2">
           Save
         </button>
         <button
