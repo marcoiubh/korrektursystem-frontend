b323d06699be84c7744bf7f92bdeb630a45eadd1
marcoiubh <marco.koll@iubh-fernstudium.de>
2022-11-16 22:38:40 +0100

    filtering, sorting, searching, paginating

--- src/components/tickets.jsx
+++ src/components/tickets.jsx
@@ -3,6 +3,10 @@ import TicketTable from './subcomponents/ticketTable';
 import { getTickets } from './services/ticketService';
 import { useNavigate } from 'react-router-dom';
 import config from '../config/config.json';
+import _ from 'lodash';
+import { paginate } from './subcomponents/paginate';
+import Pagination from './subcomponents/pagination';
+import SearchBox from './subcomponents/searchBox';
 
 const Tickets = () => {
   const navigate = useNavigate();
@@ -11,14 +15,34 @@ const Tickets = () => {
     column: 'title',
     order: 'asc',
   });
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize, setPageSize] = useState(3);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [totalCount, setTotalCount] = useState(0);
 
   useEffect(() => {
-    const fetchData = async () => {
+    const getPagedData = async () => {
       const { data: tickets } = await getTickets();
-      setTickets(tickets);
+      let filtered = tickets;
+      if (searchQuery)
+        filtered = tickets.filter((m) =>
+          m.title.toLowerCase().startsWith(searchQuery.toLowerCase())
+        );
+      const sorted = _.orderBy(
+        filtered,
+        [sortColumn.column],
+        [sortColumn.order]
+      );
+      const tickets_paginated = paginate(
+        sorted,
+        currentPage,
+        pageSize
+      );
+      setTotalCount(sorted.length);
+      setTickets(tickets_paginated);
     };
-    fetchData();
-  }, []);
+    getPagedData();
+  }, [currentPage, pageSize, sortColumn, searchQuery]);
 
   const handleEdit = (ticket) => {
     navigate(`/tickets/${ticket._id}`);
@@ -32,12 +56,23 @@ const Tickets = () => {
     navigate(`/new`);
   };
 
-  const handleSort = (e) => {
-    console.log('sorted');
+  const handleSort = (sortColumn) => {
+    setSortColumn(sortColumn);
   };
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  const handleSearch = (query) => {
+    setCurrentPage(1);
+    setSearchQuery(query);
+  };
+
   return (
     <div>
       <h1>Tickets</h1>
+      <SearchBox value={searchQuery} onChange={handleSearch} />
       <TicketTable
         tickets={tickets}
         sortColumn={sortColumn}
@@ -45,6 +80,12 @@ const Tickets = () => {
         onSort={handleSort}
         onNew={handleNew}
         onView={handleView}
+      />
+      <Pagination
+        itemsCount={totalCount}
+        pageSize={pageSize}
+        currentPage={currentPage}
+        onPageChange={handlePageChange}
       />
     </div>
   );
