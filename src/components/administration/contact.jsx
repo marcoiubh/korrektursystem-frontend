c3cb6a0e94bc87abb94d85ac1fca93ad3104113b
marcoiubh <marco.koll@iubh-fernstudium.de>
2022-12-15 11:12:34 +0100

    implement debounce for issue

--- src/components/administration/contact.jsx
+++ src/components/administration/contact.jsx
@@ -1,5 +1,5 @@
 import { useForm } from 'react-hook-form';
-import React from 'react';
+import React, { useMemo } from 'react';
 import { joiResolver } from '@hookform/resolvers/joi';
 import InputHook from '../subcomponents/atomicHooks/InputHook';
 import Button from '../subcomponents/atomic/button';
@@ -8,6 +8,7 @@ import TextAreaHook from '../subcomponents/atomicHooks/textAreaHook';
 import { toast } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
 import { sendEmail } from '../../services/emailService';
+import _ from 'lodash';
 
 const Contact = () => {
   const navigate = useNavigate();
@@ -33,13 +34,22 @@ const Contact = () => {
     } catch (ex) {}
   };
 
+  const debouncedHandleSend = useMemo(
+    () =>
+      _.debounce(handleSend, 1000, {
+        leading: true,
+        trailing: false,
+      }),
+    []
+  );
+
   return (
     <div className="container">
       <h1>Contact</h1>
 
       <form
         className="col-sm-4 mt-lg-5"
-        onSubmit={handleSubmit(handleSend)}
+        onSubmit={handleSubmit(debouncedHandleSend)}
       >
         <InputHook
           property="issue"
