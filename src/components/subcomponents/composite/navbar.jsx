1e06a61d2c742f6123500c95da57da689bbbbae8
marcoiubh <marco.koll@iubh-fernstudium.de>
2022-12-19 11:00:59 +0100

    refactor: navbar

--- src/components/subcomponents/composite/navbar.jsx
+++ src/components/subcomponents/composite/navbar.jsx
@@ -14,18 +14,18 @@ const NavBar = ({ user }) => {
       >
         Tickets
       </NavLink>
-      {!user && (
-        <NavLink className="navbar-brand active " to="/login">
-          Login
-        </NavLink>
-      )}
-      {user && (
+
+      {user ? (
         <div>
           <span className="navbar-brand active ">User: {user}</span>
           <NavLink className="navbar-brand active " to="/logout">
             Logout
           </NavLink>
         </div>
+      ) : (
+        <NavLink className="navbar-brand active " to="/login">
+          Login
+        </NavLink>
       )}
     </nav>
   );
