3d9685d0bd503d3fe223f8de37d1dcb13974fcc5
marcoiubh <marco.koll@iubh-fernstudium.de>
2022-12-29 15:57:19 +0100

    implement: role dependend columns

--- src/config/propertyList.js
+++ src/config/propertyList.js
@@ -1,10 +1,9 @@
-import Button from '../components/subcomponents/atomic/button';
 import { ifUserIsStudent } from '../services/authenticationService';
 import { getFormattedTimestamp } from '../services/getFormattedTimestamp';
 import '../css/ticketTable.css';
 
 const getPropertyList = () => {
-  return [
+  const student = [
     {
       name: 'date',
       content: (ticket) => (
@@ -28,22 +27,46 @@ const getPropertyList = () => {
     {
       name: 'status',
     },
-    {
-      name: ifUserIsStudent() ? undefined : 'student',
+  ];
 
+  const professor = [
+    {
+      name: 'date',
       content: (ticket) => (
-        // <label className="doNotDisplay">{ticket.student}</label>
-        <label hidden={ifUserIsStudent()}>{ticket.student}</label>
+        <span>{getFormattedTimestamp(ticket.date)}</span>
       ),
     },
-    {
-      name: ifUserIsStudent() ? undefined : 'priority',
 
-      content: (ticket) => (
-        <label hidden={ifUserIsStudent()}>{ticket.priority}</label>
-      ),
+    {
+      name: 'priority',
+
+      content: (ticket) => <label>{ticket.priority}</label>,
+    },
+    {
+      name: 'student',
+
+      content: (ticket) => <label>{ticket.student}</label>,
+    },
+    {
+      name: 'title',
+    },
+
+    {
+      name: 'module',
+    },
+    {
+      name: 'type',
+    },
+    {
+      name: 'source',
+    },
+    {
+      name: 'status',
     },
   ];
+
+  if (ifUserIsStudent()) return student;
+  else return professor;
 };
 
 export default getPropertyList;
