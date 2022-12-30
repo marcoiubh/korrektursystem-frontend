8e4cda9993c06a9e6754014aed279f9d5e734bb9
marcoiubh <marco.koll@iubh-fernstudium.de>
2022-12-30 18:39:09 +0100

    fix: tab select order

--- src/components/ticket/ticketOverview.jsx
+++ src/components/ticket/ticketOverview.jsx
@@ -23,12 +23,6 @@ const TicketOverview = ({
 }) => {
   return (
     <div className="app_ticketOverview">
-      <div className="app_items">
-        <DropDown
-          label={'Results per page'}
-          onClick={onItemCountChange}
-        />
-      </div>
       <div className="app_pagination">
         <Pagination
           itemsCount={totalCount}
@@ -37,14 +31,20 @@ const TicketOverview = ({
           onPageChange={onPageChange}
         />
       </div>
-      <div className="app_search">
-        <SearchBox value={searchQuery} onChange={onSearch} />
-      </div>
       <div className="app_create">
         <Button
           label="Create Ticket"
           onClick={onNew}
           hidden={!ifUserIsStudent()}
+        />
+      </div>
+      <div className="app_search">
+        <SearchBox value={searchQuery} onChange={onSearch} />
+      </div>
+      <div className="app_items">
+        <DropDown
+          label={'Results per page'}
+          onClick={onItemCountChange}
         />
       </div>
       <div className="app_table">
