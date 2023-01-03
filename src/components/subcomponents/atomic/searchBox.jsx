047fb660cf2719f578eed45ded38f2a2cfa9c8b7
marcoiubh <marco.koll@iubh-fernstudium.de>
2023-01-03 20:36:32 +0100

    refactor: SearchBox

--- src/components/subcomponents/atomic/searchBox.jsx
+++ src/components/subcomponents/atomic/searchBox.jsx
@@ -4,10 +4,8 @@ const SearchBox = ({ value, onChange }) => {
   return (
     <input
       className="search"
-      name="query"
       onChange={(e) => onChange(e.currentTarget.value)}
       placeholder="Search..."
-      type="text"
       value={value}
     />
   );
