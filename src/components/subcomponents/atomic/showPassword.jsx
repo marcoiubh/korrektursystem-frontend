8bf0e265fdf6aa89a9ac1e99bd244d727340eba3
marcoiubh <116795622+marcoiubh@users.noreply.github.com>
2023-01-05 02:02:39 +0100

    Delete showPassword.jsx

--- src/components/subcomponents/atomic/showPassword.jsx
+++
@@ -1,8 +0,0 @@
-const ShowPassword = ({ state, onClick }) => {
-  let classes = 'fa fa-solid fa-eye';
-  if (state) classes += '-slash';
-
-  return <i onClick={onClick} className={classes} />;
-};
-
-export default ShowPassword;
