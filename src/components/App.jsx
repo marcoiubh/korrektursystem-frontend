1b72b35b4e643e65fff407b606d1e945fd3a2bb7
marcoiubh <marco.koll@iubh-fernstudium.de>
2023-01-06 22:32:38 +0100

    refactor: index, app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,16 +13,21 @@ import ExpiredSession from './administration/ExpiredSession';
 import useRefresh from '../services/useRefresh';
 
 function App() {
+  // set global background-color
   document.body.style.backgroundColor = '#242629';
 
+  // auto refresh every second
   const time = useRefresh();
 
+  // disable notification queue
   toast.clearWaitingQueue();
 
+  // get user object
   let user = getCurrentUser();
 
   return (
     <div className="container">
+      {/* notification */}
       <ToastContainer
         transition={Slide}
         limit={1}
@@ -37,29 +42,42 @@ function App() {
         pauseOnHover
         theme="dark"
       />
+
+      {/* navigation bar */}
       <NavBar user={user} />
 
+      {/* valid user */}
       {user ? (
         <Routes>
+          {/* arbitrary urls */}
           <Route
             path="*"
             element={<Navigate to="/ticket/overview" />}
           />
+          {/* /ticket urls */}
           <Route
             path="/ticket/*"
             element={<TicketController user={user} time={time} />}
           />
+
+          {/* new ticket - students only */}
           {user.role === 'student' ? (
             <Route path="/new" element={<NewTicket user={user} />} />
           ) : null}
+
+          {/* contact */}
           <Route path="/issue" element={<Issue />} />
         </Routes>
       ) : (
+        // non-valid users
         <Routes>
+          {/* expired token */}
           <Route
             path="/expiredSession"
             element={<ExpiredSession />}
           />
+
+          {/* arbitrary urls */}
           <Route path="/*" element={<Login />} />
         </Routes>
       )}
--- src/index.jsx
+++ src/index.jsx
@@ -5,8 +5,12 @@ import App from './components/App';
 import { BrowserRouter } from 'react-router-dom';
 import 'font-awesome/css/font-awesome.min.css';
 
+// create root object
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// render react application
 root.render(
+  // use react router
   <BrowserRouter>
     <App />
   </BrowserRouter>
